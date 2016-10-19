f7c19550fd5f828163706e710c6cd6d19d0776b8
ta.tanaka <takayahilton@gmail.com>
2016-10-20 02:20:30 +0900

    map 実装

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-enum Validation<T, > {
+#[derive(Debug)]
+enum Validation<A, E> {
+    Success(A),
+    Failure(Vec<E>)
+}
 
+impl <A, E> Validation<A, E> {
+    fn map<B, F: FnOnce(A) -> B>(self, func: F) -> Validation<B, E> {
+        match self {
+            Validation::Success(a) => Validation::Success(func(a)),
+            Validation::Failure(v) => Validation::Failure(v)
+        }
+    }
 }
 
 
 
 fn main() {
-    println!("Hello, world!");
+    let suc = Validation::Success::<u32, String>(1).map(|i| i * 2);
+    println!("{:?}", suc);
 }
