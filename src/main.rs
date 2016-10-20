2ae83128f39dfd57428fd4ee3764bfef5798089f
ta.tanaka <takayahilton@gmail.com>
2016-10-20 12:35:34 +0900

    ap実装

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,27 @@ enum Validation<A, E> {
     Failure(Vec<E>)
 }
 
-impl <A, E> Validation<A, E> {
+impl<A, E: Clone> Validation<A, E> {
+
     fn map<B, F: FnOnce(A) -> B>(self, func: F) -> Validation<B, E> {
         match self {
             Validation::Success(a) => Validation::Success(func(a)),
+            Validation::Failure(v) => Validation::Failure(v)
+        }
+    }
+
+    fn ap<B, F: FnOnce(A) -> B>(self, func: Validation<F, E>) -> Validation<B, E> {
+        match (self, func) {
+            (Validation::Success(a), Validation::Success(f)) => Validation::Success(f(a)),
+            (Validation::Failure(v), Validation::Success(_)) => Validation::Failure(v),
+            (Validation::Success(_), Validation::Failure(v)) => Validation::Failure(v),
+            (Validation::Failure(v1), Validation::Failure(v2)) => Validation::Failure(v1.iter().chain(v2.iter()).map(|e| e.clone()).collect()),
+        }
+    }
+
+    fn flat_map<B, F: FnOnce(A) -> Validation<B, E>>(self, func: F) -> Validation<B, E> {
+        match self {
+            Validation::Success(a) => func(a),
             Validation::Failure(v) => Validation::Failure(v)
         }
     }
@@ -16,6 +33,12 @@ impl <A, E> Validation<A, E> {
 
 
 fn main() {
-    let suc = Validation::Success::<u32, String>(1).map(|i| i * 2);
+    use Validation::*;
+
+    let suc = Failure::<u32, &str>(vec!["fail1"])
+        .ap(Failure::<u32, &str>(vec!["fail2"])
+            .ap(Success(move |i| move |j| i + j)
+            )
+        );
     println!("{:?}", suc);
 }
